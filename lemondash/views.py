639bc5a66ab86f9dbe1b8d8958ed52c4a1f5e178
Rob Jackson <rob@rjackson.me>
2015-01-02 12:24:58 +0000

    Layout

--- lemondash/views.py
+++ lemondash/views.py
@@ -1,7 +1,8 @@
 from . import app
+from flask import render_template
 
 
 @app.route('/')
 def index():
     """ Index page for lemondash. TODO: Explain what it shows """
-    return "Hello world"
+    return render_template("dashboard.html")
